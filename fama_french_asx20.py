d294ea06306e77eaf5f0b268d9d642fe4777f21e
Bouddha0357 <kevin_sparks@orange.fr>
2025-05-14 22:49:45 +1000

    Update fama_french_asx20.py

--- fama_french_asx20.py
+++ fama_french_asx20.py
@@ -1,9 +1,1 @@
-import requests
-try:
-    response = requests.get("https://finance.yahoo.com")
-    if response.status_code == 200:
-        print("Successfully connected to Yahoo Finance!")
-    else:
-        print(f"Connection failed with status code {response.status_code}")
-except Exception as e:
-    print(f"An error occurred while connecting: {e}")
+print("Script is starting...")
